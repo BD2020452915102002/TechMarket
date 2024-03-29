268960bd3094d26bbeb96f3d370caebbe9cef796
Takeitiz <tuandat8103@gmail.com>
2024-03-29 09:34:47 +0700

    feat: add product router

--- Backend/routes/ProductRouter.js
+++ Backend/routes/ProductRouter.js
@@ -1,6 +1,7 @@
 const express = require("express")
+const { createProduct, getProductById, getAllProducts, updateProduct, deleteProduct } = require("../controllers/ProductController.js");
 const router = express.Router()
 
-router.route("/").get(getAllUsers).post(createUser);
-router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+router.route("/").get(getAllProducts).post(createProduct);
+router.route("/:id").get(getProductById).put(updateProduct).delete(deleteProduct);
 module.exports = router;
