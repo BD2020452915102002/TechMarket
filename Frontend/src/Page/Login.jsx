04ef9fb89c2ce22b59cef97c3a8dec634d56c177
Đăng Đức <95329674+BD2020452915102002@users.noreply.github.com>
2024-03-30 10:41:14 +0700

    feat : Create page login

---
+++ Frontend/src/Page/Login.jsx
@@ -0,0 +1,13 @@
+function Login() {
+    return (
+        <div className={' w-full h-[100vh] flex  justify-center items-center bg-amber-50'}>
+            <div className={'h-[600px] w-[1000px] bg-white  rounded-xl shadow-[0_0_20px] shadow-gray-500 '}>
+                <div>
+                    <h1></h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
