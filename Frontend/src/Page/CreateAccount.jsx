d8f5c35780b34c003d56b46ad6bcf90a99fb2a88
poetwritecode <datphamdang7602@gmail.com>
2024-03-30 11:48:22 +0700

    feat:updateUI

---
+++ Frontend/src/Page/CreateAccount.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CreateAccount = () => {
+    return (
+        <div className=''>
+            <div className=''></div>
+            <div className=''></div>
+
+
+
+        </div>
+    );
+};
+
+export default CreateAccount;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Login from "./Page/Login.jsx";
+import CreateAccount from './Page/CreateAccount.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter >
             <Routes>
-                <Route path={"/"} element={<Login/>}/>
+                <Route path={"/"} element={<CreateAccount />} />
             </Routes>
         </BrowserRouter>
     </React.StrictMode>,
