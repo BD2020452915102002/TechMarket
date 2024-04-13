00f5320f59c302f44c84fabb8cdbada5f00bb471
Phùng Tiến Đạt <96523185+Puda14@users.noreply.github.com>
2024-04-13 18:25:17 +0700

    feat: avatar user

--- Backend/controllers/UserController.js
+++ Backend/controllers/UserController.js
@@ -1,13 +1,39 @@
 const userService = require("../services/UserService");
+const cloudinary = require("../utils/cloudinary");
 
 exports.createUser = async (req, res) => {
+
+    var avatarPublicId;
+
     try {
         if (!req.body || !req.body.email) {
             throw new Error("Email is required.");
         }
+
+        if (req.body.avatar) {
+            const uploadedResponse = await cloudinary.uploader.upload(req.body.avatar, {
+                upload_preset: "TechMarket-User",
+            });
+            if (!uploadedResponse) {
+                throw new Error("error: can't upload image to cloudinary");
+            }
+            req.body.avatar = uploadedResponse;
+            avatarPublicId = uploadedResponse.public_id
+        }
+
         const user = await userService.createUser(req.body);
-        res.json({ data: user, status: "success" });
+        res.status(200).json({ data: user, status: "success" });
     } catch (err) {
+        try {
+            if (avatarPublicId) {
+                const destroyResponse = await cloudinary.uploader.destroy(avatarPublicId);
+                if (!destroyResponse.result === "ok") {
+                    throw new Error("Failed to delete image from Cloudinary");
+                }
+            }
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
         res.status(500).json({ error: err.message });
     }
 };
@@ -31,9 +57,41 @@ exports.getAllUsers = async (req, res) => {
 }
 
 exports.updateUser = async (req, res) => {
+
     try {
-        const user = await userService.updateUser(req.params.id, req.body);
-        res.json({ data: user, status: "success" });
+        if (req.body.avatar) {
+            user = await userService.getUserById(req.params.id);
+
+            if (!user) {
+                return res.status(404).json({ error: "User not found" });
+            }
+
+            if (user.avatar && user.avatar.public_id) {
+                await cloudinary.uploader.destroy(user.avatar.public_id);
+            }
+
+            const uploadedResponse = await cloudinary.uploader.upload(req.body.avatar, {
+                upload_preset: "TechMarket-User",
+            });
+
+            if (!uploadedResponse) {
+                throw new Error("Failed to upload image to cloudinary");
+            }
+
+            const updatedUser = await userService.updateUser(req.params.id, {
+                $set: {
+                    ...req.body,
+                    avatar: uploadedResponse,
+                },
+            }, { new: true });
+
+            res.status(200).json({ data: updatedUser, status: "success" });
+        } else {
+            const updatedUser = await userService.updateUser(req.params.id, {
+                $set: req.body,
+            }, { new: true });
+            res.status(200).json({ data: updatedUser, status: "success" });
+        }
     } catch (err) {
         res.status(500).json({ error: err.message })
     }
@@ -41,8 +99,20 @@ exports.updateUser = async (req, res) => {
 
 exports.deleteUser = async (req, res) => {
     try {
-        const user = await userService.deleteUser(req.params.id);
-        res.json({ data: user, status: "success" });
+        const user = await userService.getUserById(req.params.id);
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        if (user.avatar && user.avatar.public_id) {
+            const destroyResponse = await cloudinary.uploader.destroy(user.avatar.public_id);
+            if (!destroyResponse.result === "ok") {
+                throw new Error("Failed to delete image from Cloudinary");
+            }
+        }
+        const deletedUser = await userService.deleteUser(req.params.id);
+        res.status(200).json({ data: deletedUser, status: "success" });
     } catch (err) {
         res.status(500).json({ error: err.message })
     }
