6f1818d49abc7d3ac47f35b1c5309b527d890b5e
Đăng Đức <95329674+BD2020452915102002@users.noreply.github.com>
2024-05-09 23:04:48 +0700

    feat:update UI

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,6 +6,8 @@ import {BrowserRouter} from "react-router-dom";
 import { Provider } from "react-redux";
 import store, {persistor} from "../src/store/store.js";
 import {PersistGate} from "redux-persist/integration/react";
+import 'react-toastify/dist/ReactToastify.css'
+import Toastify from "./component/Toastify/Toastify.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
@@ -13,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <PersistGate loading={null} persistor={persistor}>
             <BrowserRouter>
                <AppRouters/>
+                <Toastify/>
             </BrowserRouter>
             </PersistGate>
         </Provider>
--- Frontend/src/routes/AppRouters.jsx
+++ Frontend/src/routes/AppRouters.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
+import React, {useEffect} from "react";
+import {Route, Routes, useLocation} from "react-router-dom";
 import Home from "../page/customer/Home.jsx";
 import Login from "../page/Login.jsx";
 import PrivateRoute from "./PrivateRoute.jsx";
@@ -8,72 +8,69 @@ import DetailProduct from "../page/customer/DetailProduct.jsx";
 import CreateAccount from "../page/CreateAccount.jsx";
 import ShoppingCart from "../page/customer/ShoppingCart.jsx";
 import Chatboard from "../page/customer/Chatboard/Chatboard.jsx";
-import { productApi } from "../../api/productApi.js";
-import { fetchData } from "../store/actions/productsAction.js";
-import { useDispatch, useSelector } from "react-redux";
+import {productApi} from "../../api/productApi.js";
+import {fetchData} from "../store/actions/productsAction.js";
+import {useDispatch, useSelector} from "react-redux";
 import eventEmitter from "../utils/eventEmitter.js";
 
 function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
+    const {pathname} = useLocation();
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+    return null;
 }
+
 function AppRouters(props) {
-  const dispatch = useDispatch();
-  useEffect(() => {
+    const dispatch = useDispatch();
     const fetchDataAsync = async () => {
-      try {
-        const res = await productApi.getProduct();
-        dispatch(fetchData(res.data.data));
-      } catch (error) {
-        console.error("Error:", error);
-      }
+        try {
+            const res = await productApi.getProduct();
+            dispatch(fetchData(res.data.data));
+        } catch (error) {
+            console.error("Error:", error);
+        }
     };
 
-    fetchDataAsync();
-  }, []);
-  const products = useSelector((state) => state.products.data);
+    useEffect(() => {
+        fetchDataAsync();
+    }, []);
+    return (
+        <div>
+            <ScrollToTop/>
+            <Routes>
+                <Route path={"/login"} element={<Login/>}/>
+                <Route path={"/createAccount"} element={<CreateAccount/>}/>
+                <Route path={"/cart"} element={<ShoppingCart/>}/>
+                <Route path={"/"} element={<Home/>}/>
 
-  console.log(">>>>", products);
-  return (
-    <div>
-      <ScrollToTop />
-      <Routes>
-        <Route path={"/createAccount"} element={<CreateAccount />} />
-        <Route path={"/cart"} element={<ShoppingCart />} />
-        <Route path={"/"} element={<Home />} />
-        <Route path={"/login"} element={<Login />} />
-        <Route
-          path={"/category/:categoryID"}
-          element={
-            <PrivateRoute>
-              <CategoryItem />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path={"/products/:productID"}
-          element={
-            <PrivateRoute>
-              <DetailProduct />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path={"/chat"}
-          element={
-            <PrivateRoute>
-              <Chatboard />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </div>
-  );
+                <Route
+                    path={"/category/:categoryID"}
+                    element={
+                        <PrivateRoute>
+                            <CategoryItem/>
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path={"/products/:productID"}
+                    element={
+                        <PrivateRoute>
+                            <DetailProduct/>
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path={"/chat"}
+                    element={
+                        <PrivateRoute>
+                            <Chatboard/>
+                        </PrivateRoute>
+                    }
+                />
+            </Routes>
+        </div>
+    );
 }
 
 export default AppRouters;
