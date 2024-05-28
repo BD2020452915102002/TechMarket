680f7adcb7d15323313ff7778bc49b73c100b736
Takeitiz <tuandat8103@gmail.com>
2024-05-28 15:39:53 +0700

    feat: add checkout api

--- Frontend/api/productApi.js
+++ Frontend/api/productApi.js
@@ -18,3 +18,44 @@ export const productApi = {
     return api.delete(url);
   },
 };
+
+export const userApi = {
+  getAllUsers() {
+    const url = "/user";
+    return api.get(url);
+  },
+
+  async createUser(data) {
+    try {
+    } catch (error) {
+      console.error("Error during creating user:", error);
+    }
+  },
+};
+
+export const checkoutApi = {
+  async checkout(data) {
+    try {
+      // {
+      //     "userId": "664cc642e3de3cee7ce68cb3",
+      //     "cartItems": [
+      //       {
+      //           "id": "66125699a6aeb7c988b75e06",
+      //           "quantity" : 2
+      //       },
+      //       {
+      //           "id": "66134c4c5e06ddba8e42df76",
+      //           "quantity" : 4
+      //       }
+      //     ]
+      //}
+      const url = "/stripe/create-checkout-session";
+
+      const checkoutUrl = await api.get(url, data);
+
+      window.location.href = checkoutUrl;
+    } catch (error) {
+      console.error("Error during checkout:", error);
+    }
+  },
+};
