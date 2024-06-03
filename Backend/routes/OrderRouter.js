836660c2243816b4193c66118f6029587b61113a
Phùng Tiến Đạt <96523185+Puda14@users.noreply.github.com>
2024-06-03 22:49:46 +0700

    feat: api order stats for fe

--- Backend/routes/OrderRouter.js
+++ Backend/routes/OrderRouter.js
@@ -1,136 +1,31 @@
 const express = require("express")
 const router = express.Router()
-const moment = require("moment");
-const Order = require("../models/order.js"); // Import your Order model
-const { createOrder, getAllOrders, getOrderById, updateOrder, deleteOrder } = require("../controllers/OrderController")
+const { createOrder,
+    getAllOrders,
+    getOrderById,
+    updateOrder,
+    deleteOrder,
+    getOrderStats,
+    getIncomeStats,
+    getOneWeekSales,
+    getOrderByUserId, } = require("../controllers/OrderController")
 const { isEmployee, isManager, isCustomer } = require("../middleware/auth")
 const { checkOrderAccess } = require("../middleware/AccessOrder")
 
 router.route("/").get(isEmployee, getAllOrders).post(createOrder);
 
 //GET ORDERS STATS
-router.get("/stats", isManager, async (req, res) => {
-    const previousMonth = moment()
-        .month(moment().month() - 5)
-        .set("data", 1)
-        .format("YYYY-MM-DD HH:mm:ss");
-
-    try {
-
-        // User MongoDB Aggregation Operations
-        const orders = await Order.aggregate([
-            {
-                $match: { createdAt: { $gte: new Date(previousMonth) } },
-            },
-            {
-                $project: {
-                    month: { $month: "$createdAt" },
-                },
-            },
-            {
-                $group: {
-                    _id: "$month",
-                    total: { $sum: 1 },
-                },
-            },
-        ]);
-
-        res.status(200).send(orders);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send(err);
-    }
-});
-
+router.get("/stats", isManager, getOrderStats);
 
 //GET INCOME STATS
-router.get("/income/stats", isManager, async (req, res) => {
-    const previousMonth = moment()
-        .month(moment().month() - 5)
-        .set("data", 1)
-        .format("YYYY-MM-DD HH:mm:ss");
-
-    try {
-        // User MongoDB Aggregation Operations
-
-        const income = await Order.aggregate([
-            {
-                $match: { createdAt: { $gte: new Date(previousMonth) } },
-            },
-            {
-                $project: {
-                    month: { $month: "$createdAt" },
-                    sales: "$total",
-                },
-            },
-            {
-                $group: {
-                    _id: "$month",
-                    total: { $sum: "$sales" },
-                },
-            },
-        ]);
-
-        res.status(200).send(income);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send(err);
-    }
-});
+router.get("/income/stats", isManager, getIncomeStats);
 
 //GET 1 WEEK SALES
-router.get("/week-sales", isManager, async (req, res) => {
-    const last7Days = moment()
-        .day(moment().day() - 7)
-        .format("YYYY-MM-DD HH:mm:ss");
-
-    try {
-        // User MongoDB Aggregation Operations
-
-        const income = await Order.aggregate([
-            {
-                $match: { createdAt: { $gte: new Date(last7Days) } },
-            },
-            {
-                $project: {
-                    day: { $dayOfWeek: "$createdAt" },
-                    sales: "$total",
-                },
-            },
-            {
-                $group: {
-                    _id: "$day",
-                    total: { $sum: "$sales" },
-                },
-            },
-        ]);
-
-        res.status(200).send(income);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send(err);
-    }
-});
+router.get("/week-sales", isManager, getOneWeekSales);
 
 //GET USER ORDERS
-router.get("/find/:userId", isEmployee, async (req, res) => {
-    try {
-        const orders = await Order.find({ userId: req.params.userId });
-        res.status(200).send(orders);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
+router.get("/find/:userId", isEmployee, getOrderByUserId);
 
-
-router.get("/findOne/:id", isEmployee, async (req, res) => {
-    try {
-        const order = await Order.findById(req.params.id);
-        res.status(200).send(order);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
 
 router.route("/:id").get(isEmployee, getOrderById).put(checkOrderAccess, updateOrder).delete(isEmployee, deleteOrder);
 
