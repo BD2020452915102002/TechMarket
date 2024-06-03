087109cc9cc1976cc0bf78e759fac1c0843c1b51
poetwritecode <datphamdang7602@gmail.com>
2024-06-03 22:07:44 +0700

    feat:update routes

--- Frontend/src/page/customer/ConfirmEmail.jsx
+++ Frontend/src/page/customer/ConfirmEmail.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from "../../component/Navbar.jsx";
 import Footer from "../../component/Footer.jsx";
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { Link } from "react-router-dom";
 function ConfirmEmail(props) {
     return (
         <div className='bg-gray-50'>
@@ -12,9 +13,11 @@ function ConfirmEmail(props) {
                     <div className='flex justify-center items-center text-green-500'><CheckCircleIcon /></div>
                     <h2 className="text-2xl font-bold text-gray-800 mt-4">Đăng ký thành công!</h2>
                     <p className="text-gray-600 mt-2">Cảm ơn bạn đã đăng ký. Sử dụng tài khoản để đăng nhập và trải nghiệm ngay.</p>
-                    <button className="mt-6 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition duration-200">
-                        Trở về trang chủ
-                    </button>
+                    <Link to={'/'}>
+                        <button className="mt-6 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition duration-200">
+                            Trở về trang chủ
+                        </button>
+                    </Link>
                 </div>
             </div>
             <Footer />
