26324edad106a92d779a6d8386fa37d041f40ecb
Đăng Đức <95329674+BD2020452915102002@users.noreply.github.com>
2024-06-06 22:00:59 +0700

    feat:update UI

--- Frontend/src/page/manage/Dashboard.jsx
+++ Frontend/src/page/manage/Dashboard.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {dashboardApi} from "../../../api/dashboardApi.js";
 
 function Dashboard() {
-
+    const fetch = async ()=>{
+        try {
+           const res = await dashboardApi.sumOrderTo5MonthRecent()
+            console.log(res.data)
+        }catch (e){
+            console.log(e)
+        }
+    }
+    useEffect(() => {
+        fetch()
+    },[])
     return (
         <div>
             <p>abasd</p>
