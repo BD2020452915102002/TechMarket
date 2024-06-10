f6b074571bc786329833dee21fa132be8ea803b4
poetwritecode <datphamdang7602@gmail.com>
2024-06-10 10:51:30 +0700

    feat: update ui

--- Frontend/src/component/comments/CommentForm.jsx
+++ Frontend/src/component/comments/CommentForm.jsx
@@ -1,38 +1,31 @@
-import { useState } from "react";
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
-const CommentForm = ({
-                         handleSubmit,
-                         submitLabel,
-                         hasCancelButton = false,
-                         handleCancel,
-                         initialText = "",
-                     }) => {
-    const [text, setText] = useState(initialText);
-    const isTextareaDisabled = text.length === 0;
+const CommentForm = ({ handleSubmit, submitLabel }) => {
+    const [text, setText] = useState("");
+
     const onSubmit = (event) => {
         event.preventDefault();
         handleSubmit(text);
         setText("");
     };
+
     return (
-        <form onSubmit={onSubmit}>
-      <textarea
-          className="comment-form-textarea"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-      />
-            <button className="comment-form-button" disabled={isTextareaDisabled}>
+        <form onSubmit={onSubmit} className="space-y-2 !relative">
+            <TextField
+                fullWidth
+                variant="outlined"
+                multiline
+                rows={2}
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="Write your comment here..."
+                required
+            />
+            <Button variant="contained" color="primary" type="submit" className={'!absolute !top-1/2  right-4 -translate-y-1/2 !m-0'}>
                 {submitLabel}
-            </button>
-            {hasCancelButton && (
-                <button
-                    type="button"
-                    className="comment-form-button comment-form-cancel-button"
-                    onClick={handleCancel}
-                >
-                    Cancel
-                </button>
-            )}
+            </Button>
         </form>
     );
 };
